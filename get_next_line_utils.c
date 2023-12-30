a1235b9d24cff287b359fa91fabe611d0137f8ed
vketteni <v.ketteniss@outlook.com>
2023-12-30 18:01:58 +0100

    created get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: vketteni <vketteni@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 14:05:52 by vketteni          #+#    #+#             */
-/*   Updated: 2023/12/29 17:01:20 by vketteni         ###   ########.fr       */
+/*   Updated: 2023/12/30 17:57:49 by vketteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,55 @@
 
 char	*ft_realloc(char *ptr, size_t new_size)
 {
-	char *new_memory;
-	int i;
+	char	*new_memory;
+	int		i;
 
 	new_memory = (char *)malloc(sizeof(char) * new_size);
 	if (new_memory == 0)
 		return (0);
-    if (ptr)
-    {
-	while (ptr[i] == '\0')
-	{
-		new_memory[i] = ptr[i];
-		i++;
-	}
-	while (i < new_size)
-		new_memory[i++] = '\0';
-    }
 	i = 0;
-
+	if (ptr)
+	{
+		while (ptr[i] != '\0')
+		{
+			new_memory[i] = ptr[i];
+			i++;
+		}
+		while (i < new_size)
+			new_memory[i++] = '\0';
+	}
 	new_memory[i] = '\0';
 	return (new_memory);
+}
+
+char	*ft_process_buffer_into_line(char *line, char *buffer, int buff_pos)
+{
+	int	line_length;
+	int	i;
+	int	j;
+
+	line_length = 0;
+	while (line[line_length])
+		line_length++;
+	i = buff_pos;
+	while (buffer[i] && buffer[i] != '\n')
+		line_length++;
+	line = ft_realloc(line, line_length + 1);
+	if (line == NULL)
+		return (NULL);
+	j = 0;
+	while (buff_pos < i)
+		line[j++] = buffer[buff_pos++];
+	return (line);
+}
+
+ssize_t	ft_read_to_buffer(int fd, char *buffer)
+{
+	ssize_t	bytes_read;
+
+	bytes_read = read(fd, buffer, BUFF_SIZE);
+	if (bytes_read <= 0)
+		return (NULL);
+	buffer[bytes_read] = '\0';
+	return (bytes_read);
 }
